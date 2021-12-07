694fbb4a3133b79b67d31e633f4573b539e76a0b
william-weber <taj.tek@gmail.com>
2021-12-07 11:53:07 -0800

    Add Upload page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import ConnectWalletButton from './components/ConnectWalletButton'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Upload from './pages/Upload'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/upload" element={<Upload />} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
   return (
     <>
       <h2>What would you like to do?</h2>
       <ul>
-        <li>Upload an image from my computer</li>
+        <li>
+          <Link to="upload">Upload an image from my computer</Link>
+        </li>
         {/*<li>Upload an image from the web</li>
         <li>Publish some text</li>
         <li>Save a webpage to a PDF on IPFS</li>
