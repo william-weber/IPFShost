e21f77d30d11237bb88913b8fedf1ab7fa136c4b
william-weber <taj.tek@gmail.com>
2021-12-07 09:07:49 -0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import './App.css'
+import { useState } from 'react'
 import ConnectWalletButton from './components/ConnectWalletButton'
+import Home from './pages/Home'
 
 function App() {
+  const [page, setPage] = useState('home')
+
+  function currentPage() {
+    if (page === 'home') {
+      return <Home goToUpload={() => setPage('upload')} />
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-          <h1>G Host</h1>
+          <h1>IPFS Host</h1>
           <ConnectWalletButton />
       </header>
-      <div className="App-body"></div>
+      <div className="App-body">
+        {currentPage()}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home(goToUpload: Function) {
+  return (
+    <>
+      <h2>What would you like to do?</h2>
+      <ul>
+        <li onClick={goToUpload}>Upload an image from my computer</li>
+        {/*<li>Upload an image from the web</li>
+        <li>Publish some text</li>
+        <li>Save a webpage to a PDF on IPFS</li>
+        <li>Make a copy of a file on IPFS</li>
+        <li>Copy a video from YouYube to IPFS</li>*/}
+      </ul>
+    </>
+  )
+}
